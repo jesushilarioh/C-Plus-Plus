6cc13cd2a045f5eb7f9d972ba9783a2b0f2e896d
jesushilarioh <jesushilariohernandez@gmail.com>
2018-05-27 19:16:08 -0500

    added challenge 14

--- Chapter-5-Loops-and-Files/Review Questions and Exercises/Programming Challenges/014.cpp
+++ Chapter-5-Loops-and-Files/Review Questions and Exercises/Programming Challenges/014.cpp
@@ -1,6 +1,6 @@
 /********************************************************************
 *
-*       014. Find as many errors as you can.
+*       014. STUDENT LINE UP
 *
 *       A teacher has asked all her students to line up single file
 *       according to their first name. For example, in one class Amy
@@ -21,9 +21,52 @@
 *
 ********************************************************************/
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    
+    // Variables
+    string student, first, last;
+    int num_of_students, i = 1;
+
+    // prompt the user to enter the number of students in the class,
+    cout << "Enter the number of students: ";
+
+    // Input Validation: Do not accept a number less than 1 or greater than 25
+    while(!(cin >> num_of_students) || (num_of_students < 1 || num_of_students > 25))
+    {
+        cout << "ERROR: Enter a number between 1 and 25: ";
+        cin.clear();
+        cin.ignore(123, '\n');
+    }
+
+    cout << "Enter the name of student No. 1: ";
+    cin >> student;
+
+    first = last = student;
+
+    if (student < first)
+        first = student;
+    if (student > last)
+        last = student;
+
+    for (int i = 1; i < num_of_students; i++)
+    {
+        cout << "Enter the name of student No. " << (i + 1) << ": ";
+        cin >> student;
+
+        if (student < first)
+            first = student;
+        if (student > last)
+            last = student;
+    }
+
+    cout << "First student in line = " << first << endl;
+    cout << "Last student in line  = " << last << endl;
+
+    // Once all the names have been read it reports which student
+    // would be at the front of the line and which one would be at the
+    // end of the line.
+
     return 0;
 }
