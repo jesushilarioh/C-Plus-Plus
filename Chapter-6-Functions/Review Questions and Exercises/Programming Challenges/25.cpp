e5ec22e79baadc955af6f00e10e398d438f4d5e2
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2019-01-01 12:35:06 -0600

    added

--- Chapter-6-Functions/Review Questions and Exercises/Programming Challenges/25.cpp
+++ Chapter-6-Functions/Review Questions and Exercises/Programming Challenges/25.cpp
@@ -81,16 +81,489 @@
  *                        arrival.
  *
  * 	Jesus Hilario Hernandez
- * 	December 27, 2018 --> "Merry Christmas!"
+ * 	December 29, 2018 --> "Merry Christmas!"
  *
  *************************************************************/
 #include <iostream>
 using namespace std;
 
+// Function Prototypes
+int daysSpent(int);
+int arrivalTime(int);
+int arrivalTime(int);
+double roundTripAirfare(double);
+double carRentalAmount(double);
+int carMilage(int);
+double vehicleExpenses(double);
+double parkingFees(double);
+double taxiFees(double);
+double conferenceFees(double conference_fees);
+double hotelExpenses(double hotel_expenses);
+double mealAmount(double meal_amount);
+void calculateAndDisplay();
+
 int main()
 {
-    // Calculate the total travel expenses expenses of a businessperson on a trip
-    // Display the total travel expenses expenses of a businessperson on a trip
+    // Variables
+    int days_spent = daysSpent(days_spent),
+        departure_time = departureTime(departure_time),
+        arrival_time = arrivalTime(arrival_time);
+    
+    double round_trip_airfare = roundTripAirfare(round_trip_airfare),
+           car_rental_amount = carRentalAmount(car_rental_amount),
+           car_milage = carMilage(car_milage),
+           vehicle_expense = vehicleExpenses(car_milage),
+           parking_fees = parkingFees(parking_fees),   // ?
+           taxi_fees = taxiFees(taxi_fees),    // ? 
+           conference_fees = conferenceFees(conference_fees),
+           hotel_expenses = hotelExpenses(hotel_expenses),
+           meal_amount = mealAmount(meal_amount);
+
+    calculateAndDisplay();
+
     return 0;
 }
 
+int daysSpent(int days_spent)
+{
+    cout << "Enter total days spent on trip: ";
+    cin >> days_spent;
+    return days_spent;
+}
+int departureTime(int departure_time)
+{
+    cout << "Enter time of departure on 1st day\n"
+         << "(use military time. For example, \n"
+         << "10:02 am = 1002,\n"
+         << "11:00 am = 1100,\n"
+         << "12:31 pm = 1231,\n"
+         << " 1:00 pm = 1300,\n"
+         << " 2:46 pm = 1446,\n"
+         << " 3:15 pm = 1515,\n"
+         << " 4:00 pm = 1600, etc...): ";
+    cin >> departure_time;
+    return departure_time;
+}
+int arrivalTime(int arrival_time)
+{
+    cout << "Enter time of arrival on last day\n"
+         << "(use military time. For example, \n"
+         << "10:02 am = 1002,\n"
+         << "11:00 am = 1100,\n"
+         << "12:31 pm = 1231,\n"
+         << " 1:00 pm = 1300,\n"
+         << " 2:46 pm = 1446,\n"
+         << " 3:15 pm = 1515,\n"
+         << " 4:00 pm = 1600, etc...): ";
+    cin >> arrival_time;
+    return arrival_time;
+}
+double roundTripAirfare(double round_trip_airfare)
+{
+    char user_choice;
+
+    cout << "Did you pay for round trip air fair? (y/n): ";
+    cin >> user_choice;
+
+    if (user_choice == 'Y' || user_choice == 'y')
+    {
+        cout << "Enter amount of round-trip airfare: ";
+        cin >> round_trip_airfare;
+        return round_trip_airfare;
+    }
+    else if (user_choice == 'N' || user_choice == 'n')
+    {
+        round_trip_airfare = 0;
+        return round_trip_airfare;
+    }
+    else
+    {
+        cout << "Error: Y or N must be chosen to proceed: ";
+    }
+    
+}
+double carRentalAmount(double car_rental_amount)
+{
+    char user_choice;
+
+    cout << "Did you rent a car? (y/n): ";
+    cin >> user_choice;
+    
+    if (user_choice == 'Y' || user_choice == 'y')
+    {
+        cout << "Enter amount of car rental: ";
+        cin >> car_rental_amount;
+        return car_rental_amount;
+    }
+    else if (user_choice == 'N' || user_choice == 'n')
+    {
+        car_rental_amount = 0;
+        return car_rental_amount;
+    }
+    else
+    {
+        cout << "Error: Y or N must be chosen to proceed: ";
+    }
+    
+}
+int carMilage(int car_milage)
+{
+    char user_choice;
+
+    cout << "Did you use a personal vehicle? (y/n): ";
+    cin >> user_choice;
+    
+    if (user_choice == 'Y' || user_choice == 'y')
+    {
+        cout << "Enter miles driven in private vehicle: ";
+        cin >> car_milage;
+        return car_milage;
+    }
+    else if (user_choice == 'N' || user_choice == 'n')
+    {
+        car_milage = 0;
+        return car_milage;
+    }
+    else
+    {
+        cout << "Error: Y or N must be chosen to proceed: ";
+    }
+}
+double vehicleExpenses(double car_milage)
+{
+    return car_milage * .27;
+}
+double parkingFees(double parking_fees)
+{
+    char user_choice;
+    int parking_days;
+    double fee,
+           amount_paid_by_employee;
+
+    cout << "Any parking fees? (y/n): ";
+    cin >> user_choice;
+
+    if (user_choice == 'Y' || user_choice == 'y')
+    {
+        cout << "How many days did you pay for parking? ";
+        cin >> parking_days;
+
+        for (int i = 0; i < parking_days; i++)
+        {
+            cout << "Enter fee amount for day "
+                 << (i + 1) << ": ";
+            cin >> fee;
+
+            if (fee <= 6.00)
+                parking_fees += fee;
+            else
+                amount_paid_by_employee = fee - 6.00;
+
+        }
+
+        return parking_fees;
+        // Or
+        // return amount_paid_by_employee;
+    }
+    else if (user_choice == 'N' || user_choice == 'n')
+    {
+        parking_fees = 0;
+        return parking_fees;
+    }
+    else
+    {
+        cout << "Error: Y or N must be chosen to proceed: ";
+    }
+    
+}
+double taxiFees(double taxi_fees)
+{
+    const double TAXI_LIMIT = 10.00;
+
+    char user_choice;
+    int taxi_days;
+    double fee,
+           amount_paid_by_employee;
+
+    cout << "Any taxi fees? (y/n): ";
+    cin >> user_choice;
+
+    if (user_choice == 'Y' || user_choice == 'y')
+    {
+        cout << "How many days did you pay for a taxi? ";
+        cin >> taxi_days;
+
+        for (int i = 0; i < taxi_days; i++)
+        {
+            cout << "Enter taxi fee amount for day "
+                 << (i + 1) << ": ";
+            cin >> fee;
+
+            if (fee <= TAXI_LIMIT)
+                taxi_fees += fee;
+            else
+                amount_paid_by_employee = fee - TAXI_LIMIT;
+
+        }
+
+        return taxi_fees;
+        // Or
+        // return amount_paid_by_employee;
+    }
+    else if (user_choice == 'N' || user_choice == 'n')
+    {
+        taxi_fees = 0;
+        return taxi_fees;
+    }
+    else
+    {
+        cout << "Error: Y or N must be chosen to proceed: ";
+    }
+}
+double conferenceFees(double conference_fees)
+{
+    char user_choice;
+    cout << "Any conference or seminar registration fees? (y/n): ";
+    cin >> user_choice;
+
+    if (user_choice == 'Y' || user_choice == 'y')
+    {
+        cout << "Enter Conference or seminar registration fees: ";
+        cin >> conference_fees;
+        return conference_fees;
+    }
+    else if (user_choice == 'N' || user_choice == 'n')
+    {
+        conference_fees = 0;
+        return conference_fees;
+    }
+    else
+    {
+        cout << "Error: Y or N must be chosen to proceed: ";
+    }
+}
+double hotelExpenses(double hotel_expenses)
+{
+    const double HOTEL_LIMIT = 90.00;
+
+    char user_choice;
+    int hotel_nights;
+    double fee,
+           amount_paid_by_employee;
+
+    cout << "Any hotel expenses? (y/n): ";
+    cin >> user_choice;
+
+    if (user_choice == 'Y' || user_choice == 'y')
+    {
+        cout << "How many nights did you pay for a hotel? ";
+        cin >> hotel_nights;
+
+        for (int i = 0; i < hotel_nights; i++)
+        {
+            cout << "Enter hotel expense amount for night "
+                 << (i + 1) << ": ";
+            cin >> fee;
+
+            if (fee <= HOTEL_LIMIT)
+                hotel_expenses += fee;
+            else
+                amount_paid_by_employee = fee - HOTEL_LIMIT;
+
+        }
+
+        return hotel_expenses;
+        // Or
+        // return amount_paid_by_employee;
+    }
+    else if (user_choice == 'N' || user_choice == 'n')
+    {
+        hotel_expenses = 0;
+        return hotel_expenses;
+    }
+    else
+    {
+        cout << "Error: Y or N must be chosen to proceed: ";
+        cin >> user_choice;
+    }
+
+}
+
+double mealAmount(double meal_amount, 
+                  double departure_time, 
+                  double arrival_time)
+{
+    const double BREAKFAST_LIMIT = 9.00,
+                 LUNCH_LIMIT     = 12.00,
+                 DINNER_LIMIT    = 16.00;
+
+    char user_choice;
+
+    double fee,
+           amount_paid_by_employee;
+
+    // FOR DEPATURE!!
+    // FOR DEPATURE!!
+    // FOR DEPATURE!!
+    cout << "Upon depature of your first day, did you have a meal? ";
+    cin >> user_choice;
+    if (user_choice == 'Y' || user_choice == 'y')
+    {
+        if(departure_time < 700)   // Breakfast
+        {
+            cout << "Since your departure time was "
+                 << departure_time << ", \n" 
+                 << "What was amount for breakfast? ";
+            cin >> fee;
+
+            if (fee < BREAKFAST_LIMIT)
+            {
+                meal_amount += fee;
+            }
+            else
+            {
+                amount_paid_by_employee = fee - BREAKFAST_LIMIT;
+            }
+        }
+        else if(departure_time > 700 && departure_time < 1200)   // Lunch
+        {
+            cout << "Since your departure time was "
+                 << departure_time << ", \n" 
+                 << "What was amount for lunch? ";
+            cin >> fee;
+
+            if (fee < LUNCH_LIMIT)
+            {
+                meal_amount += fee;
+            }
+            else
+            {
+                amount_paid_by_employee = fee - LUNCH_LIMIT;
+            }
+        }
+        else if(departure_time > 1200 && departure_time < 1800)  // Dinner
+        {
+            cout << "Since your departure time was "
+                 << departure_time << ", \n" 
+                 << "What was amount for dinner? ";
+            cin >> fee;
+
+            if (fee < DINNER_LIMIT)
+            {
+                meal_amount += fee;
+            }
+            else
+            {
+                amount_paid_by_employee = fee - DINNER_LIMIT;
+            }
+        }
+        else    // If not within time restrains of Breakfast, Lunch, or Dinner.
+        {
+            cout << "Since your departure time was "
+                 << departure_time << ", \n" 
+                 << "What was amount for your meal? ";
+            cin >> fee;
+
+            amount_paid_by_employee = fee;
+        }
+    }
+    else if (user_choice == 'N' || user_choice == 'n')
+    {
+        meal_amount = 0.00;
+    }
+    else
+    {
+        cout << "Error: Y or N must be chosen to proceed: ";
+        cin >> user_choice;
+    }
+
+    // FOR ARRIVAL
+    // FOR ARRIVAL
+    // FOR ARRIVAL
+    cout << "Upon arrival on your last day, did you have a meal? ";
+    cin >> user_choice;
+    if (user_choice == 'Y' || user_choice == 'y')
+    {
+        if(arrival_time > 800 && arrival_time < 1300)   // Breakfast
+        {
+            cout << "Since your arrival time was "
+                 << arrival_time << ", \n" 
+                 << "What was amount for breakfast? ";
+            cin >> fee;
+
+            if (fee < BREAKFAST_LIMIT)
+            {
+                meal_amount += fee;
+            }
+            else
+            {
+                amount_paid_by_employee = fee - BREAKFAST_LIMIT;
+            }
+        }
+        else if(arrival_time > 1300 && arrival_time < 1900)   // Lunch
+        {
+            cout << "Since your arrival time was "
+                 << arrival_time << ", \n" 
+                 << "What was amount for lunch? ";
+            cin >> fee;
+
+            if (fee < LUNCH_LIMIT)
+            {
+                meal_amount += fee;
+            }
+            else
+            {
+                amount_paid_by_employee = fee - LUNCH_LIMIT;
+            }
+        }
+        else if(arrival_time > 1900)  // Dinner
+        {
+            cout << "Since your arrival time was "
+                 << arrival_time << ", \n" 
+                 << "What was amount for dinner? ";
+            cin >> fee;
+
+            if (fee < DINNER_LIMIT)
+            {
+                meal_amount += fee;
+            }
+            else
+            {
+                amount_paid_by_employee = fee - DINNER_LIMIT;
+            }
+        }
+        else    // If not within time restrains of Breakfast, Lunch, or Dinner.
+        {
+            cout << "Since your arrival time was "
+                 << arrival_time << ", \n" 
+                 << "What was amount for your meal? ";
+            cin >> fee;
+
+            amount_paid_by_employee = fee;
+        }
+    }
+    else if (user_choice == 'N' || user_choice == 'n')
+    {
+        meal_amount = 0.00;
+    }
+    else
+    {
+        cout << "Error: Y or N must be chosen to proceed: ";
+        cin >> user_choice;
+    }
+}
+void calculateAndDisplay()
+{
+    cout << "Total expenses incurred by businessperson: $"
+         << total_expenses;
+    
+    cout << "Total allowable expenses for the trip : $"
+         << total_allowable_expenses;
+
+    cout << "Excess to be reimbursed to businessperson: $"
+         << total_reimbursement;
+
+    cout << "Amount saved if expenses under total allowed: $"
+         << amount_saved;
+
+}
