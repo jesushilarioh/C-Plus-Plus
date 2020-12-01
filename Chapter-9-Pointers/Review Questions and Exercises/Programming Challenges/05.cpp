25933cbdd79f59a770023868509ae7fdf4207b94
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2020-12-01 09:09:58 -0600

    modified

--- Chapter-9-Pointers/Review Questions and Exercises/Programming Challenges/05.cpp
+++ Chapter-9-Pointers/Review Questions and Exercises/Programming Challenges/05.cpp
@@ -7,8 +7,10 @@
  * 
  * int doSomething(int &x, int &y) 
  * {
- *      int temp = x; x = y * 10;
- *      y = temp * 10; return x + y;
+ *      int temp = x; 
+ *      x = y * 10;
+ *      y = temp * 10; 
+ *      return x + y;
  * }
  */
 #include <iostream>
