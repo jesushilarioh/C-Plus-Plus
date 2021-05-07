c91ed230cfc43348234dded7a76d5190caf56cef
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2021-05-06 21:07:54 -0500

    added and modified

--- Chapter-10-Characters-C-Strings-and-More-about-the-string-Class/Programming Challenges/13.cpp
+++ Chapter-10-Characters-C-Strings-and-More-about-the-string-Class/Programming Challenges/13.cpp
@@ -10,10 +10,12 @@
  */
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 void validateDate(string);
+void displayDate(string);
 
 int main()
 {
@@ -21,31 +23,64 @@ int main()
 
     validateDate(user_date);
 
+    displayDate(user_date);
+
     return 0;
 } 
 void validateDate(string user_date)
 {
-    bool notValid = false;
+    bool notValid = false,
+         isNumber = false;
+    
+    int i;
 
     do
     {
+        isNumber = false;
+
+        cout << "Enter a date (mm/dd/yyyy): ";
         getline(cin, user_date);
 
-        if (user_date.length() == 10) // mm/dd/yyyy
+        // mm/dd/yyyy
+        if (user_date.length() == 10 && 
+            user_date[2] == '/' && 
+            user_date[5] == '/')  
         {
-            if (user_date[0] == '0' || user_date[0] == '1')
+            // Check for numbers in mm, dd, and yyyy
+            for (i = 0; i < user_date.length(); i++)
             {
-                
+                if (i != 2 || i != 5)
+                {
+                    if (isdigit(i))
+                        isNumber = true;
+                    else
+                    {
+                        cout << "Please enter a valid date." << endl;
+                        continue;
+                    }       
+                }       
             }
-            else
-                continue;
         }
         else 
+        {
+            cout << "Please enter a valid date." << endl;
             continue;
-        
-        
+        }
 
-
+        if(user_date[0] )
+            notValid = true;
+        
     } while (!notValid);
-    
+
+}
+
+void displayDate(string user_date)
+{
+    string month = user_date[0] + user_date[1];
+    int mm = stoi(month);
+
+    string day = user_date[3] + user_date[4];
+
+    string year = user_date[6] + user_date[7] + user_date[8] + user_date[9];
+
 }
